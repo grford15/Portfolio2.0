cfe96acd7de68b450e315623ebcd655c71c53838
Greg <g.rutherford10@gmail.com>
2019-07-22 20:35:31 +0100

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Greg's new porfolio!</h1>
-      </header>
+    <div className="App-Container">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className="App">
+    <h1>Greg's newest Portfolio</h1>
+  </div>
+);
+
+export default Header;
