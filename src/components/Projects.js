e905ddabba4aa44d66d622c3d61f4f2672f4b2eb
Greg <g.rutherford10@gmail.com>
2019-07-26 12:00:20 +0100

    add images for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import rubyScreenshot from '../ruby-app.jpeg';
+import javaScreenShot from '../restaurant-app.jpeg';
+import javascriptScreenShot from '../javascript-app.jpeg';
 import './styling/Projects.css';
 
 const Projects = () => (
   <div className="Projects-Container">
     <h4>This is where the projects will go</h4>
-    <div className="project-display">
-      <div className="project-box">
-        <p>Project One</p>
+    <div className="cc-projects">
+      <div className="cc-project-box">
+        <img src={rubyScreenshot} alt="ruby-app" />
+        <p>Ruby Project</p>
       </div>
-      <div className="project-box">
-        <p>Project Two</p>
+      <div className="cc-project-box alt-display">
+        <img src={javaScreenShot} alt="java-app" />
+        <p>Java Project</p>
       </div>
-      <div className="project-box">
-        <p>Project Three</p>
-      </div>
-      <div className="project-box">
-        <p>Project Four</p>
+      <div className="cc-project-box">
+        <img src={javascriptScreenShot} alt="javaScript-app" />
+        <p>JavaScript Project</p>
       </div>
     </div>
   </div>
